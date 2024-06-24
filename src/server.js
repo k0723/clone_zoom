40a33c98ad43ddac46d798799f62bf489cf1aa22
k0723 <kimbum7824@gmail.com>
2024-06-25 02:13:53 +0900

    1st day

--- src/server.js
+++ src/server.js
@@ -7,12 +7,29 @@ const app = express();
 app.set('view engine', 'pug');
 app.set ("views", __dirname +"/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/",(_,res) => res.render("home"));
+app.get("/",(_,res) => res.redirect("/"));
 
 
 app.get("/", (req, res) => res.render("home"));
 
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({server});
+
+wss.on("connection", (socket) => {
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer",offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice,roomName) => {
+        socket.to(roomName).emit("ice",ice);
+    });
+});
 
 server.listen(3000)
